264a48760952fc60d70ac1e935ea24d887d442dd
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2023-01-01 22:41:15 +0900

    feat: homescreen

--- App.tsx
+++ App.tsx
@@ -1,71 +1,7 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, FlatList, SafeAreaView } from 'react-native'
-import ListItem from './components/ListItem'
-import Constants from 'expo-constants'
-import { useState } from 'react'
-import { useEffect } from 'react'
-import axios from 'axios'
-
-const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${
-  Constants.manifest!.extra!.newsApiKey
-}`
-
-type Article = {
-  author: string
-  title: string
-  urlToImage: string
-  publishedAt: string
-}
+import AppNavigator from './navigation/AppNavigator'
 
 const App = () => {
-  const [articleList, setArticleList] = useState<Article[]>()
-
-  useEffect(() => {
-    const f = async () => {
-      const { isError, response } = await fetchArticleList()
-      if (!isError) setArticleList(response['data']['articles'])
-      else alert(response)
-    }
-    f()
-  }, [])
-
-  const fetchArticleList = async () => {
-    let response: any = undefined
-    let isError: boolean = false
-
-    try {
-      response = await axios.get(URL)
-    } catch (err: any) {
-      console.error(response)
-      isError = true
-    }
-
-    return { isError, response }
-  }
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={articleList}
-        renderItem={({ item }) => (
-          <ListItem
-            imageUrl={item.urlToImage}
-            title={item.title}
-            author={item.author}
-          />
-        )}
-        keyExtractor={(_, index) => index.toString()}
-      />
-      <StatusBar style='auto' />
-    </SafeAreaView>
-  )
+  return <AppNavigator />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-})
 
 export default App
