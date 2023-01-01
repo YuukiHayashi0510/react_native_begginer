494c47190eacbeaf204f426bd5a4cb7bd0159dc9
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2023-01-01 17:48:15 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          height: 100,
+          width: '100%',
+          borderColor: 'gray',
+          borderWidth: 1,
+        }}
+      >
+        <Text>Expo + TypeScript</Text>
+      </View>
+      <StatusBar style='auto' />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
